2e35cc69c27d624e54e9ffa7c091e3de91cd7a83
jeremy.comino <jeremy.comino@estudiantat.upc.edu>
2023-08-12 00:11:43 +0200

    añadido inicializacion

--- Window/Window.cc
+++ Window/Window.cc
@@ -25,7 +25,7 @@ Window::Window(){
     }
     glViewport(0,0, 800, 600);
     glfwSetFramebufferSizeCallback(window, callback);
-    c = std::chrono::high_resolution_clock::now();
+    initialize();
 }
 void Window::run(){
     while(not glfwWindowShouldClose(window)){
@@ -50,37 +50,36 @@ void Window::add_vertice(vector<glm::vec2>& vector, float x, float y){
     glm::vec2 var = glm::vec2(x,y);
     vector.push_back(var);
 }
-void Window::rendering(){
-    std::shared_ptr<Shader> vs(new VertexShader());
-    std::shared_ptr<Shader> fs(new FragmentShader());
+void Window::initialize(){
+     vs = std::shared_ptr<Shader>(new VertexShader());
+     fs = std::shared_ptr<Shader>(new FragmentShader());
     
     vs->compileSourceShader("./shaders/vshader.vert");
     fs->compileSourceShader("./shaders/fshader.frag");
     
-    std::shared_ptr<Program> p (new Program());
+     p = std::shared_ptr<Program>(new Program());
     
     p->attachShader(fs->getId());
     p->attachShader(vs->getId());
     p->compile();
 
-    vector<glm::vec3> v;
+    //init v
     add_vertice(v, -1.f,0.f,0.f);
     add_vertice(v, 1.f,0.f,0.f);
     add_vertice(v, 0.f,1.f,0.f);
-    vector<glm::vec2> v2;
+    //init v2
     add_vertice(v2,0.f,0.f);
     add_vertice(v2, 1.f,0.f);
     add_vertice(v2, 0.5f,1.f);
 
-    glClearColor(0.5f, 0.5f,0.5f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
     
-    GLuint VAO;
+    
+    
     //creamos VAO
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     
-    GLuint VBO;
+    
     glGenBuffers(1,&VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     int vertex = glGetAttribLocation(p->getId(), "vertex");
@@ -88,7 +87,7 @@ void Window::rendering(){
     glVertexAttribPointer(vertex,3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(vertex);
 
-    GLuint VBO2;
+    
     glGenBuffers(1, &VBO2);
     glBindBuffer(GL_ARRAY_BUFFER, VBO2);
     glBufferData(GL_ARRAY_BUFFER, v2.size()*sizeof(glm::vec2), &v2[0], GL_STATIC_DRAW);
@@ -99,9 +98,13 @@ void Window::rendering(){
     glBindVertexArray(0);
     
     //funfact, puedes vincular más de una imagen a una Textura y se elegirá el que este activo.
-    std::shared_ptr<Texture> t(new ImageTexture("./images/arcoiris.jpg", 1));
-    std::shared_ptr<Texture> t2(new ImageTexture("./images/facebook.png", 1));
-
+     t = std::shared_ptr<Texture>(new ImageTexture("./images/arcoiris.jpg", 1));
+     t2 = std::shared_ptr<Texture>(new ImageTexture("./images/facebook.png", 1));
+     c = std::chrono::high_resolution_clock::now();
+}
+void Window::rendering(){
+    glClearColor(0.5f, 0.5f,0.5f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     std::chrono::high_resolution_clock::time_point c2 = std::chrono::high_resolution_clock::now();
     float elapsed_time_ms = std::chrono::duration<float, std::milli>(c2-c).count();
--- Window/Window.hpp
+++ Window/Window.hpp
@@ -24,8 +24,22 @@ class Window{
 
     GLFWwindow* window;
 
+    //instance attributes
+    std::shared_ptr<Shader> vs;
+    std::shared_ptr<Shader> fs;
+    std::shared_ptr<Program> p;
+    vector<glm::vec3> v;
+    vector<glm::vec2> v2;
+    GLuint VAO;
+    GLuint VBO;
+    GLuint VBO2;
+    std::shared_ptr<Texture> t;
+    std::shared_ptr<Texture> t2;
     std::chrono::high_resolution_clock::time_point c;
+
+
     //functions
+    void initialize();
     static void callback(GLFWwindow* window, int w, int h);
     static void read_escape(GLFWwindow* window);
     static void add_vertice(vector<glm::vec3>& vector, float x, float y, float z);
